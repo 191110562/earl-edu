e0a3f8d6bb8cfbdcee66f88316d8b9efe7243dea
unknown <1y24yray@gmail.com>
2022-12-01 14:35:08 +0700

    responsive navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom';
 import{ FiLogOut } from 'react-icons/fi';
 import { useAuthContext } from "../contexts/AuthContext";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Navigation() {
   const [error, setError] = useState('')
@@ -19,16 +22,30 @@ export default function Navigation() {
     }
   }
   return (
-    <nav className='navbar'>
-        <ul>
-            <h1>EarlEdu</h1>
-            <li className='push'><NavLink to='/home'>Home</NavLink></li>
-            <li><NavLink to='/community'>Community</NavLink></li>
-            <li><NavLink to='/baby-name-finder'>Baby Name Finder</NavLink></li>
-            <li><NavLink to='/learn-alphabet'>Learn Alphabet</NavLink></li>
-            <p>{currentUser && currentUser.email}</p>
-            <li><button onClick={onLogoutHandler}><FiLogOut/></button></li>
-        </ul>
-    </nav>
+    <Navbar bg="transparent" variant="transparent">
+        <Container>
+          <Navbar.Brand href="/home">EarlEdu</Navbar.Brand>
+          <Nav className="ms-auto responsive-font">
+            <Nav.Link onClick={(e) => {
+              e.preventDefault();
+              navigate("/home");
+            }}>Home</Nav.Link>
+            <Nav.Link onClick={(e) => {
+              e.preventDefault();
+              navigate("/community");
+            }}>Community</Nav.Link>
+            <Nav.Link onClick={(e) => {
+              e.preventDefault();
+              navigate("/baby-name-finder");
+            }}>Name Finder</Nav.Link>
+            <Nav.Link onClick={(e) => {
+              e.preventDefault();
+              navigate("/learn-alphabet");
+            }}>Learn Alphabet</Nav.Link>
+            <p className='p-nav'>{currentUser && currentUser.email}</p>
+            <button className='button-trans' onClick={onLogoutHandler}><FiLogOut/></button>
+          </Nav>
+        </Container>
+    </Navbar>
   )
 }
